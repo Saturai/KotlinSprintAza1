87d1769aa2a8c8d6231ca21c91fa91252710b1d7
saturai <moirai2010@mail.ru>
2025-03-07 22:11:27 +0300

    KS-2-4 percentage change

--- src/main/kotlin/Lessons_2/Lessons2-Task4.kt
+++ src/main/kotlin/Lessons_2/Lessons2-Task4.kt
@@ -5,10 +5,10 @@ fun main() {
     val iron = 11
     val crystal = 7
 
-    val baff = 0.2 // 20% бонусного повышения
+    val baff20Percent: Float = 20/100F //20% бонусного повышения
 
-    val baffIron = (iron * baff).toInt()
-    val baffCrystal = (crystal * baff).toInt()
+    val baffIron = (iron * baff20Percent).toInt()
+    val baffCrystal = (crystal * baff20Percent).toInt()
 
     println("Железо увеличено на:$baffIron")
     println("Кристалы увеличены на:$baffCrystal")
