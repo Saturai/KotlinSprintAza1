fbd40d131a6f4869baab165df684220892d7f9b1
saturai <moirai2010@mail.ru>
2025-03-08 22:37:03 +0300

    KS-3-1 created greeting

---
+++ src/main/kotlin/Lessons_3/Lessons3-Task1.kt
@@ -0,0 +1,13 @@
+package org.example.Lessons_3
+
+fun main() {
+
+    val name = "Олег"
+
+    val goodDay = "Добрый день"
+    val goodEvening = "Добрый вечер"
+
+    println("$goodDay $name")
+    println("$goodEvening $name")
+
+}
