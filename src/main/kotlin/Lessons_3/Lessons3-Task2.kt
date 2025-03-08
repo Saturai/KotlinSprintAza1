435b93509fed0a1d6470c5b8688ac3f250af4746
saturai <moirai2010@mail.ru>
2025-03-08 22:57:02 +0300

    KS-3-2 dependence of last name on age

---
+++ src/main/kotlin/Lessons_3/Lessons3-Task2.kt
@@ -0,0 +1,18 @@
+package org.example.Lessons_3
+
+fun main() {
+
+    val firstName = "Татьяна"
+    var lastName = "Сидорова"
+    val  patronymic = "Сергеевна"
+
+    var age = 20
+
+    println("$lastName $firstName $patronymic, возраст- $age ")
+
+    lastName = "Андреева"
+    age = 22
+
+    println("$lastName $firstName $patronymic, возраст- $age")
+
+}
