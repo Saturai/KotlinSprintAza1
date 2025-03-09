370e4971d7d54d6c00349eef5f7fa101d759826f
saturai <moirai2010@mail.ru>
2025-03-09 22:17:51 +0300

    KS-1-4 changed the type of the variable

--- src/main/kotlin/Lessons_1/Lessons1-Task4.kt
+++ src/main/kotlin/Lessons_1/Lessons1-Task4.kt
@@ -6,7 +6,7 @@ fun main() {
     val age: Byte = 27
     val partOfDay: Float = 0.075F
     val second: Short = 6480
-    val partOfYear: String = "2.0547945205479453E-4"
+    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
     println("Расстояние- ${lenght}")
